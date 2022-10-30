2746cb83bc76d8758671d6405c18a029aa1f0e91
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2022-10-30 16:32:56 -0300

    Other: visual feedback

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { MainHeader } from "../components/MainHeader";
+import { ClassCard } from "../components/ClassCard";
+ 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <MainHeader />
+      <main role='main' style={{display: "flex", flexDirection: "column", marginLeft: "172px",}}>
+        <section>
+          <h4 style={{fontSize: '18px', fontWeight: 'normal', margin: '0px', marginLeft: '44px', marginTop: '96px'}}>
+            Turmas
+          </h4>
+          <div style={{display: 'flex', gap: '40px', marginTop: '30px'}} className="flex gap-10">
+            <ClassCard className="Turma 1 ssssssssqew qdqwdqrfqefqew fqw qwrfq" />
+            <ClassCard className="Turma 2" />
+            <ClassCard />
+          </div>
+        </section>
+      </main>
+    </div>
+  )
 }
 
 export default Home;
