c5fc94bf431606ae9b993c9b3f8defd1d23488ae
viniciuscosta110 <viniciuscosta1900@hotmail.com>
2022-11-20 00:32:10 -0300

    Feat create class modal

---
+++ src/components/CreateClassModal/styles.js
@@ -0,0 +1,11 @@
+export const modalStyle = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: '#FFFFFF',
+  borderRadius: '16px',
+  boxShadow: 24,
+  p: 4,
+};
